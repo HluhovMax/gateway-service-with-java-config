805544c727883ba0eb98fb4910792113b48b2a9b
holosievo@gmail.com <gshockprotection>
2019-05-09 12:40:47 +0200

    Initial commit

---
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,30 @@
+package com.example.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("client-service", r -> r
+						.path("/api/v1/client/**")
+						.uri("lb://client-service"))
+				.route("auth-service", r -> r
+						.path("/oauth/token")
+						.uri("lb://auth-service"))
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
